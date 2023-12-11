fa15910fe31d986bff7aae369922f8eb201bc9f8
Shimpei Iwamaru <gangan.education@gmail.com>
2023-12-11 17:11:52 +0900

    fix: createTemplates function

--- src/templates.ts
+++ src/templates.ts
@@ -1,25 +1,18 @@
 import { useNuxt, addTemplate } from "@nuxt/kit";
+import consola from "consola";
 import type { ModuleOptions } from "./types";
 
 /**
- *
+ * create templates for markuplint config
  * @param config
  * @param nuxt nuxt app
  */
 export function createTemplates(config: ModuleOptions, nuxt = useNuxt()) {
+  consola.info(`create ${config.configPath}`);
   // TODO: Parse config args.
   return addTemplate({
     filename: config.configPath,
-    getContents: () => `
-// @ts-check
-/**
-  * @type {import("@markuplint/ml-config").Config}
-  */
-module.exports = {
-  specs: { ${config.specs} },
-  parser: { ${config.parser} },
-  extends: ${config.extends},
-};`,
+    getContents: () => `module.exports = ${JSON.stringify(config, null, 2)};`,
     write: true,
   }).dst;
 }
